6c56ce6951b9a70c37c4240171f978a832e1ce32
Flakoooo <vlasov.vadom@gmail.com>
2026-05-17 00:25:18 +0500

    create active applications page

---
+++ CleaningAppWeb/Components/Shared/CheckboxFilter/CheckboxFilter.razor.cs
@@ -0,0 +1,78 @@
+﻿using CleaningAppWeb.Domain.DTOs;
+using Microsoft.AspNetCore.Components;
+
+namespace CleaningAppWeb.Components.Shared.CheckboxFilter
+{
+    public partial class CheckboxFilter<T>
+    {
+        [Parameter]
+        public string Label { get; set; } = string.Empty;
+
+        [Parameter]
+        public List<T> AllValues { get; set; } = [];
+
+        [Parameter]
+        public HashSet<T> SelectedValues { get; set; } = [];
+
+        [Parameter]
+        public EventCallback<HashSet<T>> SelectedValuesChanged { get; set; }
+
+        [Parameter]
+        public EventCallback FilterChanged { get; set; }
+
+        [Parameter]
+        public Func<int, Task<ListDataResponse<T>>>? DataLoaderMethod { get; set; }
+
+        private List<T> _values = [];
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (DataLoaderMethod is not null)
+            {
+                await LoadCheckboxDataAsync();
+                MarkAsInitialized();
+            }
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            if (DataLoaderMethod is not null)
+            {
+                ResetPagination();
+                await LoadCheckboxDataAsync();
+            }
+            else
+            {
+                _values = AllValues.ToList();
+            }
+        }
+
+        protected override async Task OnLoadMoreItemsAsync()
+            => await LoadCheckboxDataAsync(append: true);
+
+        protected override int GetCurrentItemsCount() => _values.Count;
+
+        private async Task LoadCheckboxDataAsync(bool append = false)
+        {
+            if (DataLoaderMethod is not null)
+            {
+                await LoadDataAsync(
+                    AllValues,
+                    () => DataLoaderMethod.Invoke(_currentPage),
+                    append: append
+                );
+
+                _values = AllValues.ToList();
+            }
+        }
+
+        private async Task SelectValue(T value)
+        {
+            if (!SelectedValues.Add(value))
+                SelectedValues.Remove(value);
+
+            await SelectedValuesChanged.InvokeAsync(SelectedValues);
+            await FilterChanged.InvokeAsync();
+        }
+    }
+}
---
+++ CleaningAppWeb/Components/Shared/Inputs/Number/Number.razor.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CleaningAppWeb.Components.Shared.Inputs.Number
+{
+    public partial class Number<T> where T : struct, IComparable, IConvertible, IComparable<T>, IEquatable<T>
+    {
+        [Parameter]
+        public string? Label { get; set; }
+
+        [Parameter]
+        public T? MinValue { get; set; }
+
+        [Parameter]
+        public T? MaxValue { get; set; }
+
+        [Parameter]
+        public string Placeholder { get; set; } = string.Empty;
+
+        [Parameter]
+        public T? Value { get; set; }
+
+        [Parameter]
+        public EventCallback<T?> ValueChanged { get; set; }
+
+        [Parameter]
+        public bool NeedValidation { get; set; } = false;
+
+        [Parameter]
+        public string? ErrorMessage { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool? IsDisabled { get; set; }
+
+        private bool _showError = false;
+
+        protected override void OnInitialized()
+        {
+            if (typeof(T).IsValueType && Value.HasValue)
+            {
+                if (MinValue.HasValue && Comparer<T>.Default.Compare(Value.Value, MinValue.Value) < 0)
+                    Value = null;
+                else if (MaxValue.HasValue && Comparer<T>.Default.Compare(Value.Value, MaxValue.Value) > 0)
+                    Value = null;
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            _showError = NeedValidation && (!Value.HasValue ||
+                (MinValue.HasValue && Comparer<T>.Default.Compare(Value.Value, MinValue.Value) < 0) ||
+                (MaxValue.HasValue && Comparer<T>.Default.Compare(Value.Value, MaxValue.Value) > 0));
+
+            ErrorMessage = $"Значение должно быть от {MinValue} до {MaxValue}";
+            StateHasChanged();
+        }
+
+        private async Task OnInputChanged(T? value)
+        {
+            if (value.HasValue)
+            {
+                if (MinValue.HasValue && Comparer<T>.Default.Compare(value.Value, MinValue.Value) < 0)
+                    value = MinValue.Value;
+                else if (MaxValue.HasValue && Comparer<T>.Default.Compare(value.Value, MaxValue.Value) > 0)
+                    value = MaxValue.Value;
+            }
+
+            if (ValueChanged.HasDelegate)
+                await ValueChanged.InvokeAsync(value);
+        }
+
+        private Dictionary<string, object> GetInputAttributes()
+        {
+            var attributes = new Dictionary<string, object>();
+
+            if (IsDisabled.HasValue && IsDisabled.Value)
+                attributes["disabled"] = "disabled";
+
+            return attributes;
+        }
+    }
+}
