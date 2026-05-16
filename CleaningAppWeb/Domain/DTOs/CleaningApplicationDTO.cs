fcb9a5f9313485a2005a69b3bc614496ea96b113
Flakoooo <vlasov.vadom@gmail.com>
2026-05-16 22:46:52 +0500

    create applications card and application card

---
+++ CleaningAppWeb/Domain/DTOs/CleaningApplicationDTO.cs
@@ -0,0 +1,24 @@
+﻿using CleaningAppWeb.Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleaningAppWeb.Domain.DTOs
+{
+    public class CleaningApplicationDTO
+    {
+        public required Guid Id { get; set; }
+        public required UserDTO Initiator { get; set; }
+        public UserDTO? Executor { get; set; }
+        public OfficeDTO? Office { get; set; }
+        public CleaningApplicationStatus Status { get; set; }
+        public string ClientFirstName { get; set; } = string.Empty;
+        public string ClientLastName { get; set; } = string.Empty;
+        public string ClientPatronymic { get; set; } = string.Empty;
+        public string ClientTelephoneNumber { get; set; } = string.Empty;
+        public required DateOnly CleaningDate { get; set; }
+        public required TimeOnly CleaningTime { get; set; }
+        public string Comment { get; set; } = string.Empty;
+        public byte Rating { get; set; } = 0;
+        public string Feedback { get; set; } = string.Empty;
+
+    }
+}
---
+++ CleaningAppWeb/Domain/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using CleaningAppWeb.Domain.Enums;
+
+namespace CleaningAppWeb.Domain.DTOs
+{
+    public class UserDTO
+    {
+        public required Guid Id { get; set; }
+        public required string Login { get; set; }
+        public RoleType Role { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Patronymic { get; set; } = string.Empty;
+        public string TelephoneNumber { get; set; } = string.Empty;
+    }
+}
